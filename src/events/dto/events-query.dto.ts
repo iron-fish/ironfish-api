126d61d3fc23504cbd0ab519754210566e0039fa
Rohan Jadvani <jadvanirohan@gmail.com>
2021-07-16 09:42:39 -0400

    feat(api): Add events controller

---
+++ src/events/dto/events-query.dto.ts
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional } from 'class-validator';
+import { PaginationArgsDto } from '../../common/dto/pagination-args.dto';
+
+export class EventsQueryDto extends PaginationArgsDto {
+  @IsInt()
+  @IsOptional()
+  @Type(() => Number)
+  readonly account_id?: number;
+}
---
+++ src/events/events.controller.spec.ts
@@ -0,0 +1,84 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { v4 as uuid } from 'uuid';
+import { PrismaService } from '../prisma/prisma.service';
+import { bootstrapTestApp } from '../test/test-app';
+import { EventType } from '.prisma/client';
+
+describe('EventsController', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    app = await bootstrapTestApp();
+    prisma = app.get(PrismaService);
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('GET /events/', () => {
+    describe('with no filters', () => {
+      it('returns a list of events', async () => {
+        const { body } = await request(app.getHttpServer())
+          .get(`/events`)
+          .expect(HttpStatus.OK);
+
+        expect((body as unknown[]).length).toBeGreaterThan(0);
+        expect((body as unknown[])[0]).toMatchObject({
+          id: expect.any(Number),
+          account_id: expect.any(Number),
+          type: expect.any(String),
+        });
+      });
+    });
+
+    describe('with an account filter', () => {
+      it('returns events only for that account', async () => {
+        const account = await prisma.account.create({
+          data: {
+            public_address: uuid(),
+          },
+        });
+        const firstEvent = await prisma.event.create({
+          data: {
+            type: EventType.BUG_CAUGHT,
+            account_id: account.id,
+          },
+        });
+        const secondEvent = await prisma.event.create({
+          data: {
+            type: EventType.COMMUNITY_CONTRIBUTION,
+            account_id: account.id,
+          },
+        });
+        const thirdEvent = await prisma.event.create({
+          data: {
+            type: EventType.SOCIAL_MEDIA_PROMOTION,
+            account_id: account.id,
+          },
+        });
+        const events = [firstEvent, secondEvent, thirdEvent];
+
+        const { body } = await request(app.getHttpServer())
+          .get(`/events`)
+          .query({ account_id: account.id })
+          .expect(HttpStatus.OK);
+
+        expect(body).toHaveLength(events.length);
+        for (const event of body) {
+          expect(event).toMatchObject({
+            id: expect.any(Number),
+            account_id: account.id,
+            type: expect.any(String),
+          });
+        }
+      });
+    });
+  });
+});
