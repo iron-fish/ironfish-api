502e9bb115aa1be0db9692d77755343a92eb521e
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2021-07-27 19:21:58 -0400

    feat(api): Adjust points with weekly limits (#50)

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,8 +1,20 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { EventType } from '.prisma/client';
+
 export const MS_PER_DAY = 1000 * 60 * 60 * 24;
 
 // Pagination limits
 export const DEFAULT_LIMIT = 20;
 export const MAX_LIMIT = 100;
+
+// Event categories
+export const WEEKLY_POINT_LIMITS_BY_EVENT_TYPE: Record<EventType, number> = {
+  [EventType.BLOCK_MINED]: 1000,
+  [EventType.BUG_CAUGHT]: 1000,
+  [EventType.COMMUNITY_CONTRIBUTION]: 1000,
+  [EventType.NODE_HOSTED]: 1000,
+  [EventType.PULL_REQUEST_MERGED]: 1000,
+  [EventType.SOCIAL_MEDIA_PROMOTION]: 1000,
+};
