327e3cde9862e2a575218471f8de567a4f7ef33d
Rohan Jadvani <jadvanirohan@gmail.com>
2021-08-11 16:13:26 -0400

    Fix events controller test

--- src/events/events.controller.spec.ts
+++ src/events/events.controller.spec.ts
@@ -145,6 +145,7 @@ describe('EventsController', () => {
             email: faker.internet.email(),
             graffiti: uuid(),
             country_code: faker.address.countryCode('alpha-3'),
+            last_login_at: new Date(),
           },
         });
         const type = EventType.BUG_CAUGHT;
