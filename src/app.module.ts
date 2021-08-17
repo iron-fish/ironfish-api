d84f5c327058a152356f7af74aef88a1e77b1178
Rohan Jadvani <jadvanirohan@gmail.com>
2021-08-17 19:14:45 -0400

    feat(api): Add middleware to require SSL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import joi from 'joi';
 import { AuthModule } from './auth/auth.module';
 import { AuthRestModule } from './auth/auth.rest.module';
 import { BlocksModule } from './blocks/blocks.module';
 import { BlocksRestModule } from './blocks/blocks.rest.module';
+import { RequireSslMiddleware } from './common/middlewares/require-ssl.middleware';
 import { EventsModule } from './events/events.module';
 import { EventsRestModule } from './events/events.rest.module';
 import { HealthModule } from './health/health.module';
@@ -35,4 +36,8 @@ import { UsersRestModule } from './users/users.rest.module';
     UsersRestModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(RequireSslMiddleware);
+  }
+}
