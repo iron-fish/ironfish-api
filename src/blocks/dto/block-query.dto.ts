e302ceeea8c165bf0c634a9d863cf4574487ea4f
adcrn <9167686+adcrn@users.noreply.github.com>
2021-08-19 19:22:28 -0400

    IRO-974: Add better validation on BlockQueryDto

--- src/blocks/dto/block-query.dto.ts
+++ src/blocks/dto/block-query.dto.ts
@@ -2,14 +2,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Type } from 'class-transformer';
-import { IsInt, IsString, Min } from 'class-validator';
+import { IsDefined, IsInt, IsString, Min, ValidateIf } from 'class-validator';
 
 export class BlockQueryDto {
+  @ValidateIf((o: BlockQueryDto) => o.sequence === undefined)
+  @IsDefined({
+    message: '"hash" or "sequence" required to query for single block'
+  })
   @IsString()
-  readonly hash!: string;
+  readonly hash?: string;
 
+  @ValidateIf((o: BlockQueryDto) => o.hash === undefined)
+  @IsDefined({
+    message: '"hash" or "sequence" required to query for single block'
+  })
   @Min(1)
   @IsInt()
   @Type(() => Number)
-  readonly sequence!: number;
+  readonly sequence?: number;
 }
