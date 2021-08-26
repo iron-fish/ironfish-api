229e589dde4f19675440cf491bdd8b3f7bf11486
Rohan Jadvani <jadvanirohan@gmail.com>
2021-08-26 16:00:02 -0400

    feat(api): Add `occurred_at` when creating events

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -2,7 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Type } from 'class-transformer';
-import { IsEnum, IsInt, IsString, Min } from 'class-validator';
+import {
+  IsDate,
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
 import { EventType } from '.prisma/client';
 
 export class CreateEventDto {
@@ -16,4 +23,9 @@ export class CreateEventDto {
 
   @IsEnum(EventType)
   readonly type!: EventType;
+
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  readonly occurred_at?: Date;
 }
