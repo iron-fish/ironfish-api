4b1bf6b3979175059edeefbb3fc7a5d33e54624a
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2021-09-03 13:21:20 -0400

    chore(api): Remove unused command (#170)

--- package-scripts.js
+++ package-scripts.js
@@ -19,9 +19,6 @@ module.exports = {
     start: {
       script: 'node build/main.js',
       worker: 'node build/worker.js',
-      local: {
-        script: 'LOCAL=true nps start',
-      }
     },
     test: 'jest --runInBand',
   },
