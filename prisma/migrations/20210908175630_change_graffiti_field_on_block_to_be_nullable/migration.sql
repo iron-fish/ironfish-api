e4d633462b5d31ff926b2c8c4b02aed90cfc5b0e
Alexander Decurnou <alex@deekerno.com>
2021-09-08 13:57:33 -0400

    IRO-1043: Fix graffiti nullability

---
+++ prisma/migrations/20210908175630_change_graffiti_field_on_block_to_be_nullable/migration.sql
@@ -0,0 +1,32 @@
+-- DropForeignKey
+ALTER TABLE "events" DROP CONSTRAINT "FK__events__block_id";
+
+-- DropForeignKey
+ALTER TABLE "events" DROP CONSTRAINT "FK__events__user_id";
+
+-- DropForeignKey
+ALTER TABLE "transactions" DROP CONSTRAINT "transactions_block_id_fkey";
+
+-- AlterTable
+ALTER TABLE "blocks" ALTER COLUMN "graffiti" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "events" ADD CONSTRAINT "events_block_id_fkey" FOREIGN KEY ("block_id") REFERENCES "blocks"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "events" ADD CONSTRAINT "events_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_block_id_fkey" FOREIGN KEY ("block_id") REFERENCES "blocks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- RenameIndex
+ALTER INDEX "uq_blocks_on_hash_and_network_version" RENAME TO "blocks_hash_network_version_key";
+
+-- RenameIndex
+ALTER INDEX "uq_events_on_block_id" RENAME TO "events_block_id_key";
+
+-- RenameIndex
+ALTER INDEX "uq_transactions_on_hash_and_network_version" RENAME TO "transactions_hash_network_version_key";
+
+-- RenameIndex
+ALTER INDEX "uq_users_on_confirmation_token" RENAME TO "users_confirmation_token_key";
