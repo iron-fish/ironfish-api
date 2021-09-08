13c1f93996fcd93c9f9fab9f1678377801c52cc0
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2021-09-08 18:14:49 -0400

    fix(api): Filter for graffiti after computing ranks (#193)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -203,10 +203,9 @@ export class UsersService {
             ) user_latest_events
           ON
             user_latest_events.user_id = users.id
-          WHERE
-            graffiti ILIKE $1
         ) user_ranks
       WHERE
+        graffiti ILIKE $1 AND
         CASE WHEN $2
           THEN
             rank > $3
