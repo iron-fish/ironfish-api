e102be83a87ea4155a2d0d1a79b3c2ac9fd4f916
Alexander Decurnou <alex@deekerno.com>
2021-09-14 13:28:59 -0400

    -n IRO-1116: Remove unused services from BlocksTransaction service

--- src/blocks-transactions/blocks-transactions.module.ts
+++ src/blocks-transactions/blocks-transactions.module.ts
@@ -2,14 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Module } from '@nestjs/common';
-import { ApiConfigModule } from '../api-config/api-config.module';
-import { EventsModule } from '../events/events.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { BlocksTransactionsService } from './blocks-transactions.service';
 
 @Module({
   exports: [BlocksTransactionsService],
-  imports: [ApiConfigModule, EventsModule, PrismaModule],
+  imports: [PrismaModule],
   providers: [BlocksTransactionsService],
 })
 export class BlocksTransactionsModule {}
