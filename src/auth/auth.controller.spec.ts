36614daf2b1601ea6d83c2404cfda5ce3c533bbe
Rohan Jadvani <jadvanirohan@gmail.com>
2021-10-27 15:10:23 -0400

    feat(api): Migrate callback logic to login

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -6,20 +6,17 @@ import faker from 'faker';
 import request from 'supertest';
 import { ulid } from 'ulid';
 import { v4 as uuid } from 'uuid';
-import { ApiConfigService } from '../api-config/api-config.service';
 import { MagicLinkService } from '../magic-link/magic-link.service';
 import { bootstrapTestApp } from '../test/test-app';
 import { UsersService } from '../users/users.service';
 
 describe('AuthController', () => {
   let app: INestApplication;
-  let config: ApiConfigService;
   let magicLinkService: MagicLinkService;
   let usersService: UsersService;
 
   beforeAll(async () => {
     app = await bootstrapTestApp();
-    config = app.get(ApiConfigService);
     magicLinkService = app.get(MagicLinkService);
     usersService = app.get(UsersService);
     await app.init();
@@ -54,10 +51,62 @@ describe('AuthController', () => {
 
         const { body } = await request(app.getHttpServer())
           .post('/login')
-          .set('Authorization', 'valid-token')
+          .set('Authorization', 'invalid-token')
           .expect(HttpStatus.UNAUTHORIZED);
 
         expect(body).toMatchSnapshot();
+      });
+    });
+
+    describe('with no users for the given e-mail', () => {
+      it('returns a 401 back with `user_invalid`', async () => {
+        jest
+          .spyOn(magicLinkService, 'getEmailFromHeader')
+          .mockImplementationOnce(() => Promise.resolve('iron@fish.com'));
+
+        const { body } = await request(app.getHttpServer())
+          .post('/login')
+          .set('Authorization', 'missing-token')
+          .expect(HttpStatus.UNAUTHORIZED);
+
+        expect(body).toMatchObject({
+          error: 'user_invalid',
+        });
+      });
+    });
+
+    describe('with an unconfirmed user for the given e-mail', () => {
+      it('returns a 401 back with `user_unconfirmed`', async () => {
+        const email = 'iron@fish.com';
+        jest
+          .spyOn(magicLinkService, 'getEmailFromHeader')
+          .mockImplementationOnce(() => Promise.resolve(email));
+        jest.spyOn(usersService, 'listByEmail').mockResolvedValueOnce([
+          {
+            id: 0,
+            created_at: new Date(),
+            updated_at: new Date(),
+            email,
+            graffiti: ulid(),
+            total_points: 0,
+            country_code: faker.address.countryCode('alpha-3'),
+            email_notifications: false,
+            last_login_at: null,
+            discord: null,
+            telegram: ulid(),
+            confirmation_token: ulid(),
+            confirmed_at: new Date(),
+          },
+        ]);
+
+        const { body } = await request(app.getHttpServer())
+          .post('/login')
+          .set('Authorization', 'unconfirmed-token')
+          .expect(HttpStatus.UNAUTHORIZED);
+
+        expect(body).toMatchObject({
+          error: 'user_unconfirmed',
+        });
       });
     });
 
@@ -81,136 +130,6 @@ describe('AuthController', () => {
           .expect(HttpStatus.OK);
 
         expect(updateLastLoginAt).toHaveBeenCalledTimes(1);
-      });
-    });
-  });
-
-  describe('GET /auth/callback', () => {
-    describe('with an error fetching the email from Magic Link', () => {
-      it('redirects back with `user_invalid`', async () => {
-        jest
-          .spyOn(magicLinkService, 'getEmailFromToken')
-          .mockImplementationOnce(() => {
-            throw new Error();
-          });
-
-        const { header } = await request(app.getHttpServer())
-          .get('/auth/callback?magic_credential=yeet')
-          .expect(HttpStatus.FOUND);
-
-        expect((header as Record<string, unknown>).location).toBe(
-          `${config.get<string>(
-            'INCENTIVIZED_TESTNET_URL',
-          )}/callback?error=user_invalid`,
-        );
-      });
-    });
-
-    describe('with no users for the given e-mail', () => {
-      it('redirects back with `user_invalid`', async () => {
-        jest
-          .spyOn(magicLinkService, 'getEmailFromToken')
-          .mockImplementationOnce(() => Promise.resolve('iron@fish.com'));
-
-        const { header } = await request(app.getHttpServer())
-          .get('/auth/callback?magic_credential=yeet')
-          .expect(HttpStatus.FOUND);
-
-        expect((header as Record<string, unknown>).location).toBe(
-          `${config.get<string>(
-            'INCENTIVIZED_TESTNET_URL',
-          )}/callback?error=user_invalid`,
-        );
-      });
-    });
-
-    describe('with an unconfirmed user for the given e-mail', () => {
-      it('redirects back with `user_unconfirmed`', async () => {
-        const email = 'iron@fish.com';
-        jest
-          .spyOn(magicLinkService, 'getEmailFromToken')
-          .mockImplementationOnce(() => Promise.resolve(email));
-        jest.spyOn(usersService, 'listByEmail').mockResolvedValueOnce([
-          {
-            id: 0,
-            created_at: new Date(),
-            updated_at: new Date(),
-            email,
-            graffiti: ulid(),
-            total_points: 0,
-            country_code: faker.address.countryCode('alpha-3'),
-            email_notifications: false,
-            last_login_at: null,
-            discord: null,
-            telegram: ulid(),
-            confirmation_token: ulid(),
-            confirmed_at: new Date(),
-          },
-        ]);
-
-        const { header } = await request(app.getHttpServer())
-          .get('/auth/callback?magic_credential=yeet')
-          .expect(HttpStatus.FOUND);
-
-        expect((header as Record<string, unknown>).location).toBe(
-          `${config.get<string>(
-            'INCENTIVIZED_TESTNET_URL',
-          )}/callback?error=user_unconfirmed`,
-        );
-      });
-    });
-
-    describe('with a confirmed user', () => {
-      it('updates the last login timestamp', async () => {
-        const magicCredential = 'yeet';
-        const email = faker.internet.email();
-        const user = await usersService.create({
-          email,
-          graffiti: uuid(),
-          country_code: faker.address.countryCode('alpha-3'),
-        });
-        await usersService.confirm(user);
-
-        jest
-          .spyOn(magicLinkService, 'getEmailFromToken')
-          .mockImplementationOnce(() => Promise.resolve(email));
-
-        const { header } = await request(app.getHttpServer())
-          .get(`/auth/callback?magic_credential=${magicCredential}`)
-          .expect(HttpStatus.FOUND);
-
-        expect((header as Record<string, unknown>).location).toBe(
-          `${config.get<string>(
-            'INCENTIVIZED_TESTNET_URL',
-          )}/callback?magic_credential=${magicCredential}`,
-        );
-      });
-
-      it('redirects back with the `magic_credential`', async () => {
-        const magicCredential = 'yeet';
-        const email = faker.internet.email();
-        const user = await usersService.create({
-          email,
-          graffiti: uuid(),
-          country_code: faker.address.countryCode('alpha-3'),
-        });
-        await usersService.confirm(user);
-
-        jest
-          .spyOn(magicLinkService, 'getEmailFromToken')
-          .mockImplementationOnce(() => Promise.resolve(email));
-        const updateLastLoginAt = jest.spyOn(usersService, 'updateLastLoginAt');
-
-        await request(app.getHttpServer())
-          .get(`/auth/callback?magic_credential=${magicCredential}`)
-          .expect(HttpStatus.FOUND);
-
-        expect(updateLastLoginAt).toHaveBeenCalledTimes(1);
-        expect(updateLastLoginAt).toHaveBeenCalledWith(
-          expect.objectContaining({
-            id: user.id,
-          }),
-        );
       });
     });
   });
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,80 +3,55 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import {
   Controller,
-  Get,
   HttpStatus,
   Post,
-  Query,
+  Req,
   Res,
-  UseGuards,
-  ValidationPipe,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { Response } from 'express';
-import { ParsedUrlQueryInput, stringify } from 'querystring';
-import { ApiConfigService } from '../api-config/api-config.service';
-import { Context } from '../common/decorators/context';
-import { MagicLinkContext } from '../common/interfaces/magic-link-context';
+import { Request, Response } from 'express';
 import { MagicLinkService } from '../magic-link/magic-link.service';
 import { UsersService } from '../users/users.service';
-import { MagicLinkCallbackDto } from './dto/magic-link-callback.dto';
-import { MagicLinkGuard } from './guards/magic-link.guard';
 
 @Controller()
 export class AuthController {
   constructor(
-    private readonly config: ApiConfigService,
     private readonly magicLinkService: MagicLinkService,
     private readonly usersService: UsersService,
   ) {}
 
   @Post('login')
-  @UseGuards(MagicLinkGuard)
-  async login(
-    @Context() { user }: MagicLinkContext,
-    @Res() res: Response,
-  ): Promise<void> {
-    await this.usersService.updateLastLoginAt(user);
-    res.sendStatus(HttpStatus.OK);
-  }
-
-  @Get('auth/callback')
-  async callback(
-    @Query(
-      new ValidationPipe({
-        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
-      }),
-    )
-    { magic_credential: magicCredential }: MagicLinkCallbackDto,
-    @Res() res: Response,
-  ): Promise<void> {
-    let queryParameters: ParsedUrlQueryInput = {};
+  async login(@Req() req: Request, @Res() res: Response): Promise<void> {
     let email;
+    let errorBody = {};
+
+    const { authorization } = req.headers;
+    if (!authorization) {
+      throw new UnauthorizedException();
+    }
 
     try {
-      email = await this.magicLinkService.getEmailFromToken(magicCredential);
+      email = await this.magicLinkService.getEmailFromHeader(authorization);
     } catch {
-      queryParameters = { error: 'user_invalid' };
+      throw new UnauthorizedException();
     }
 
     if (email) {
       const user = await this.usersService.findConfirmedByEmail(email);
       if (user) {
         await this.usersService.updateLastLoginAt(user);
-        queryParameters = { magic_credential: magicCredential };
       } else {
         const unconfirmedUsers = await this.usersService.listByEmail(email);
         if (unconfirmedUsers.length > 0) {
-          queryParameters = { error: 'user_unconfirmed' };
+          errorBody = { error: 'user_unconfirmed' };
         } else {
-          queryParameters = { error: 'user_invalid' };
+          errorBody = { error: 'user_invalid' };
         }
+
+        throw new UnauthorizedException(errorBody);
       }
     }
 
-    res.redirect(
-      `${this.config.get<string>(
-        'INCENTIVIZED_TESTNET_URL',
-      )}/callback?${stringify(queryParameters)}`,
-    );
+    res.sendStatus(HttpStatus.OK);
   }
 }
