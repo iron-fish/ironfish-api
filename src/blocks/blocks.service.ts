9063e9f8a11cb4ea5c453b2f8e3af1419944af8d
Rohan Jadvani <jadvanirohan@gmail.com>
2021-11-17 20:37:09 -0500

    fix(api): Update graffiti count to check against ''

--- src/blocks/blocks.service.ts
+++ src/blocks/blocks.service.ts
@@ -263,7 +263,7 @@ export class BlocksService {
         FLOOR(COALESCE(EXTRACT(EPOCH FROM MAX(timestamp) - MIN(timestamp)), 0) * 1000 / COALESCE(COUNT(*), 1)) AS average_block_time_ms,
         COALESCE(FLOOR(AVG(difficulty) * 1000), 0) AS average_difficulty_millis,
         COUNT(*) AS blocks_count,
-        COALESCE(SUM(CASE WHEN graffiti IS NOT NULL THEN 1 ELSE 0 END), 0) AS blocks_with_graffiti_count,
+        COALESCE(SUM(CASE WHEN graffiti != '' THEN 1 ELSE 0 END), 0) AS blocks_with_graffiti_count,
         COALESCE(MAX(sequence), 0) AS chain_sequence,
         COALESCE(SUM(transactions_count), 0) AS transactions_count,
         COUNT(DISTINCT graffiti) AS unique_graffiti
