3b2db40ad678146aa2a1d3021f2743e55a460f7c
Alexander Decurnou <alex@deekerno.com>
2021-12-08 18:46:19 -0500

    Add nullable difficulty column with bigint

--- prisma/migrations/20211208232112_add_bigint_difficulty_on_blocks/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `difficulty` to the `blocks` table without a default value. This is not possible if the table is not empty.
-
-*/
-
--- AlterTable
-ALTER TABLE "blocks" ADD COLUMN     "difficulty" BIGINT NOT NULL;
