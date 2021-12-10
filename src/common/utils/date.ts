b0784ce93420ecb5f2215037b4e1fe39373b9bc8
Rohan Jadvani <jadvanirohan@gmail.com>
2021-12-10 15:23:50 -0500

    fix(api): Fix off by 1 day bug for calculating Monday

--- src/common/utils/date.ts
+++ src/common/utils/date.ts
@@ -2,8 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 export function getMondayFromDate(date: Date = new Date()): Date {
+  // Days are 0-based, so Monday is 1. On Tuesday (2) we
+  // want the offset to be 1, and Sunday we want the offset to be 6.
+  const dayOffset = (date.getUTCDay() + 6) % 7;
   const monday = new Date(
-    new Date(date).setUTCDate(date.getUTCDate() - date.getUTCDay()),
+    new Date(date).setUTCDate(date.getUTCDate() - dayOffset),
   );
   monday.setUTCHours(0, 0, 0, 0);
   return monday;
