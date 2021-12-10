b019052f67bbaf732f793f000414da5bbed91d13
Rohan Jadvani <jadvanirohan@gmail.com>
2021-12-10 11:33:06 -0500

    feat(api): Add timing and logs to worker microservice

--- src/worker.ts
+++ src/worker.ts
@@ -1,16 +1,23 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { ApiConfigService } from './api-config/api-config.service';
 import { AppModule } from './app.module';
+import { DatadogService } from './datadog/datadog.service';
 import { GraphileWorkerMicroservice } from './graphile-worker/graphile-worker.microservice';
+import { LoggerService } from './logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ApiConfigService);
+  const datadogService = app.get(DatadogService);
+  const logger = app.get(LoggerService);
 
   const graphileWorkerMicroservice = new GraphileWorkerMicroservice(
-    app.get(ConfigService),
+    config,
+    datadogService,
+    logger,
   );
   app.connectMicroservice({
     strategy: graphileWorkerMicroservice,
