96bbf7b92355726c10372e292a916b2734589eb8
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2021-12-10 16:21:55 -0500

    feat(api): Add timing and logs to worker microservice (#467)

--- src/graphile-worker/graphile-worker.microservice.ts
+++ src/graphile-worker/graphile-worker.microservice.ts
@@ -1,22 +1,26 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Injectable } from '@nestjs/common';
 import { CustomTransportStrategy, Server } from '@nestjs/microservices';
 import { run, Runner, Task, TaskList } from 'graphile-worker';
 import { Pool, PoolConfig } from 'pg';
 import { ApiConfigService } from '../api-config/api-config.service';
+import { DatadogService } from '../datadog/datadog.service';
+import { LoggerService } from '../logger/logger.service';
 import { GraphileWorkerPattern } from './enums/graphile-worker-pattern';
 import { GraphileWorkerHandler } from './types/graphile-worker-handler';
 
-@Injectable()
 export class GraphileWorkerMicroservice
   extends Server
   implements CustomTransportStrategy
 {
   private runner!: Runner;
 
-  constructor(private readonly config: ApiConfigService) {
+  constructor(
+    private readonly config: ApiConfigService,
+    private readonly datadogService: DatadogService,
+    private readonly loggerService: LoggerService,
+  ) {
     super();
   }
 
@@ -55,17 +59,53 @@ export class GraphileWorkerMicroservice
 
   private createMessageHandler(pattern: GraphileWorkerPattern): Task {
     return async (payload: unknown) => {
-      const handler = this.messageHandlers.get(
-        pattern.toString(),
-      ) as GraphileWorkerHandler;
-      if (!handler) {
-        throw new Error(`Undefined pattern: '${pattern}'`);
-      }
+      const start = new Date().getTime();
+      try {
+        await this.handle(pattern, payload);
 
-      const response = await handler(payload as Record<string, unknown>);
-      if (response && response.requeue) {
-        throw new Error('Retry Graphile Worker message');
+        this.datadogService.increment('worker.success', 1, {
+          pattern,
+        });
+      } catch (error) {
+        if (error instanceof Error) {
+          this.datadogService.increment('worker.error', 1, {
+            pattern,
+            type: error.constructor.name,
+          });
+        }
+
+        throw error;
+      } finally {
+        const duration = new Date().getTime() - start;
+
+        this.datadogService.timing('worker', duration, {
+          pattern,
+        });
+
+        this.loggerService.info(
+          JSON.stringify({
+            duration,
+            pattern,
+          }),
+        );
       }
     };
+  }
+
+  private async handle(
+    pattern: GraphileWorkerPattern,
+    payload: unknown,
+  ): Promise<void> {
+    const handler = this.messageHandlers.get(
+      pattern.toString(),
+    ) as GraphileWorkerHandler;
+    if (!handler) {
+      throw new Error(`Undefined pattern: '${pattern}'`);
+    }
+
+    const response = await handler(payload as Record<string, unknown>);
+    if (response && response.requeue) {
+      throw new Error('Retry Graphile Worker message');
+    }
   }
 }
