9c8f8e8e1bf26a3b5ccdde9df968dbaea9b006d8
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2021-12-15 15:03:50 -0500

    feat(api): Return block metadata with events (#484)

--- src/events/interfaces/event-with-metadata.ts
+++ src/events/interfaces/event-with-metadata.ts
@@ -1,15 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { EventType } from '.prisma/client';
+import { Event } from '.prisma/client';
 
-export interface SerializedEvent {
-  object: 'event';
-  id: number;
-  type: EventType;
-  occurred_at: string;
-  points: number;
-  user_id: number;
+export interface EventWithMetadata extends Event {
   metadata: {
     [key: string]: unknown;
   };
