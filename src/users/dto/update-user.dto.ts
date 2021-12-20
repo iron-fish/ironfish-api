524bb03fc5ae1f09db7de18a1d86b69a0ed02f30
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2021-12-20 15:13:46 -0500

    feat(api): Add `country_code` when updating user (#500)

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,14 +1,34 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { IsDefined, IsString, ValidateIf } from 'class-validator';
+import {
+  IsDefined,
+  IsISO31661Alpha3,
+  IsString,
+  ValidateIf,
+} from 'class-validator';
 
 const UPDATE_USER_VALIDATION_MESSAGE =
-  '"discord", "graffiti", or "telegram" required when updating';
+  '"discord", "graffiti", "telegram", or "country_code" required when updating';
 
 export class UpdateUserDto {
   @ValidateIf(
-    (o: UpdateUserDto) => o.graffiti === undefined && o.telegram === undefined,
+    (o: UpdateUserDto) =>
+      o.discord === undefined &&
+      o.graffiti === undefined &&
+      o.telegram === undefined,
+  )
+  @IsDefined({
+    message: UPDATE_USER_VALIDATION_MESSAGE,
+  })
+  @IsISO31661Alpha3()
+  readonly country_code?: string;
+
+  @ValidateIf(
+    (o: UpdateUserDto) =>
+      o.country_code === undefined &&
+      o.graffiti === undefined &&
+      o.telegram === undefined,
   )
   @IsDefined({
     message: UPDATE_USER_VALIDATION_MESSAGE,
@@ -17,7 +37,10 @@ export class UpdateUserDto {
   readonly discord?: string;
 
   @ValidateIf(
-    (o: UpdateUserDto) => o.discord === undefined && o.telegram === undefined,
+    (o: UpdateUserDto) =>
+      o.country_code === undefined &&
+      o.discord === undefined &&
+      o.telegram === undefined,
   )
   @IsDefined({
     message: UPDATE_USER_VALIDATION_MESSAGE,
@@ -26,7 +49,10 @@ export class UpdateUserDto {
   readonly graffiti?: string;
 
   @ValidateIf(
-    (o: UpdateUserDto) => o.discord === undefined && o.graffiti === undefined,
+    (o: UpdateUserDto) =>
+      o.country_code === undefined &&
+      o.discord === undefined &&
+      o.graffiti === undefined,
   )
   @IsDefined({
     message: UPDATE_USER_VALIDATION_MESSAGE,
