276bb0855267dd89cd21f90a4be5968418a7d9c4
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2021-12-23 18:05:55 -0500

    refactor(api): Wrap Block find with service method (#522)

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,12 +3,13 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Module } from '@nestjs/common';
 import { ApiConfigModule } from '../api-config/api-config.module';
+import { BlocksModule } from '../blocks/blocks.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { EventsService } from './events.service';
 
 @Module({
   exports: [EventsService],
-  imports: [ApiConfigModule, PrismaModule],
+  imports: [ApiConfigModule, BlocksModule, PrismaModule],
   providers: [EventsService],
 })
 export class EventsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,13 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Module } from '@nestjs/common';
-import { EventsModule } from '../events/events.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UsersService } from './users.service';
 
 @Module({
   exports: [UsersService],
-  imports: [EventsModule, PrismaModule],
+  imports: [PrismaModule],
   providers: [UsersService],
 })
 export class UsersModule {}
