816e7c664b1e5699354f081ab210e36597189149
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2021-12-24 17:38:45 -0500

    feat(api): Add environment variables for connection limits and workers (#524)

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -2,10 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Module } from '@nestjs/common';
+import { ApiConfigModule } from '../api-config/api-config.module';
 import { PrismaService } from './prisma.service';
 
 @Module({
   exports: [PrismaService],
+  imports: [ApiConfigModule],
   providers: [PrismaService],
 })
 export class PrismaModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,12 +3,34 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ApiConfigService } from '../api-config/api-config.service';
+
+const DEFAULT_CONNECTION_LIMIT = 10;
+const DEFAULT_POOL_TIMEOUT = 10;
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor(readonly config: ApiConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: `${config.get<string>(
+            'DATABASE_URL',
+          )}?connection_limit=${config.getWithDefault(
+            'CONNECTION_LIMIT',
+            DEFAULT_CONNECTION_LIMIT,
+          )}&pool_timeout=20${config.getWithDefault(
+            'POOL_TIMEOUT',
+            DEFAULT_POOL_TIMEOUT,
+          )}`,
+        },
+      },
+    });
+  }
+
   async onModuleInit(): Promise<void> {
     await this.$connect();
   }
