2e386d030c041c5d153cc1a2d2963aa0b07c7df0
Rohan Jadvani <jadvanirohan@gmail.com>
2022-02-17 11:23:10 -0500

    feat(api): Add minimum telemetry version check

--- src/telemetry/telemetry.controller.ts
+++ src/telemetry/telemetry.controller.ts
@@ -9,11 +9,14 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
+import { gte, valid } from 'semver';
 import { InfluxDbService } from '../influxdb/influxdb.service';
 import { WriteTelemetryPointsDto } from './dto/write-telemetry-points.dto';
 
 @Controller('telemetry')
 export class TelemetryController {
+  private readonly MINIMUM_TELEMETRY_VERSION = '0.1.23';
+
   constructor(private readonly influxDbService: InfluxDbService) {}
 
   @ApiExcludeEndpoint()
@@ -29,6 +32,11 @@ export class TelemetryController {
   ): void {
     const options = [];
     for (const { fields, measurement, tags, timestamp } of points) {
+      const version = tags.find((tag) => tag.name === 'version');
+      if (!version || !this.isValidTelemetryVersion(version.value)) {
+        continue;
+      }
+
       options.push({
         fields,
         measurement,
@@ -37,6 +45,16 @@ export class TelemetryController {
       });
     }
 
-    this.influxDbService.writePoints(options);
+    if (options.length) {
+      this.influxDbService.writePoints(options);
+    }
+  }
+
+  private isValidTelemetryVersion(version: string): boolean {
+    const parsed = valid(version);
+    if (!parsed) {
+      return false;
+    }
+    return gte(parsed, this.MINIMUM_TELEMETRY_VERSION);
   }
 }
