cc742be0e5396a3310b4310808b2873639984aef
Rohan Jadvani <jadvanirohan@gmail.com>
2022-03-14 18:09:08 -0400

    Address nit

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -298,7 +298,7 @@ export class UsersService {
     );
     // If fetching a previous page, the ranks are sorted in opposite order.
     // Reverse the data so the returned chunk is in ascending order.
-    if (before !== undefined) {
+    if (!before) {
       data.reverse();
     }
 
