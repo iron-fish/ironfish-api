318ca018da0823ac93fbff3385e22a7d88a031f9
Mat <mat@ironfish.network>
2022-04-11 11:47:37 -0700

    Add unit tests

---
+++ src/node-uptimes/node-uptimes.service.spec.ts
@@ -0,0 +1,129 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { INestApplication } from '@nestjs/common';
+import assert from 'assert';
+import faker from 'faker';
+import { v4 as uuid } from 'uuid';
+import { PrismaService } from '../prisma/prisma.service';
+import { bootstrapTestApp } from '../test/test-app';
+import { NodeUptimesService } from './node-uptimes.service';
+
+describe('NodeUptimesService', () => {
+  let app: INestApplication;
+  let nodeUptimesService: NodeUptimesService;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    app = await bootstrapTestApp();
+    nodeUptimesService = app.get(NodeUptimesService);
+    prisma = app.get(PrismaService);
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  const createUser = async () => {
+    return await prisma.user.create({
+      data: {
+        email: faker.internet.email(),
+        graffiti: uuid(),
+        country_code: faker.address.countryCode(),
+      },
+    });
+  };
+
+  describe('upsert', () => {
+    it('creates node uptime if not already created', async () => {
+      const now = new Date();
+      const user = await createUser();
+
+      const uptime = await nodeUptimesService.upsert(user);
+
+      expect(uptime).toMatchObject({
+        user_id: user.id,
+        last_checked_in: expect.any(Date),
+        total_hours: 0,
+      });
+      assert(uptime);
+      expect(uptime.last_checked_in.getTime()).toBeGreaterThanOrEqual(
+        now.getTime(),
+      );
+    });
+
+    it('updates an existing node uptime if enough time has passed', async () => {
+      const now = new Date();
+      const twoHoursAgo = new Date();
+      twoHoursAgo.setHours(now.getHours() - 2);
+      const user = await createUser();
+      await prisma.nodeUptime.create({
+        data: {
+          user_id: user.id,
+          last_checked_in: twoHoursAgo,
+          total_hours: 0,
+        },
+      });
+
+      const uptime = await nodeUptimesService.upsert(user);
+
+      expect(uptime).toMatchObject({
+        user_id: user.id,
+        last_checked_in: expect.any(Date),
+        total_hours: 1,
+      });
+      assert(uptime);
+      expect(uptime.last_checked_in.getTime()).toBeGreaterThanOrEqual(
+        now.getTime(),
+      );
+    });
+
+    it('does not update node uptime if enough time has not passed', async () => {
+      const now = new Date();
+      const user = await createUser();
+      await prisma.nodeUptime.create({
+        data: {
+          user_id: user.id,
+          last_checked_in: now,
+          total_hours: 0,
+        },
+      });
+
+      const uptime = await nodeUptimesService.upsert(user);
+      const dbUptime = await nodeUptimesService.get(user);
+
+      expect(uptime).toBeNull();
+
+      expect(dbUptime).toMatchObject({
+        user_id: user.id,
+        last_checked_in: now,
+        total_hours: 0,
+      });
+    });
+  });
+
+  describe('get', () => {
+    it('returns null if not created', async () => {
+      const user = await createUser();
+
+      const result = await nodeUptimesService.get(user);
+
+      expect(result).toBeNull();
+    });
+
+    it('returns node uptime if created', async () => {
+      const user = await createUser();
+
+      const uptime = await prisma.nodeUptime.create({
+        data: {
+          user_id: user.id,
+        },
+      });
+
+      const result = await nodeUptimesService.get(user);
+
+      expect(result).toEqual(uptime);
+    });
+  });
+});
