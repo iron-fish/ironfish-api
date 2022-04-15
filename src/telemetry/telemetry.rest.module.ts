b03709c95089caeacdc156a2a6fc799121359bb3
Mat <mat@ironfish.network>
2022-04-15 10:52:09 -0700

    Quick fix to avoid tracking uptime prematurely

--- src/telemetry/telemetry.rest.module.ts
+++ src/telemetry/telemetry.rest.module.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Module } from '@nestjs/common';
+import { ApiConfigModule } from '../api-config/api-config.module';
 import { InfluxDbModule } from '../influxdb/influxdb.module';
 import { NodeUptimesModule } from '../node-uptimes/node-uptimes.module';
 import { UsersModule } from '../users/users.module';
@@ -9,6 +10,6 @@ import { TelemetryController } from './telemetry.controller';
 
 @Module({
   controllers: [TelemetryController],
-  imports: [InfluxDbModule, NodeUptimesModule, UsersModule],
+  imports: [ApiConfigModule, InfluxDbModule, NodeUptimesModule, UsersModule],
 })
 export class TelemetryRestModule {}
