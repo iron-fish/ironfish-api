d7a1ab95b0aeb13df8ca98a275fceb54e01d839f
Rohan Jadvani <jadvanirohan@gmail.com>
2022-05-05 17:09:32 -0400

    feat(api): Remove `users.total_points`

---
+++ prisma/migrations/20220505210849_remove_total_points_from_users/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `total_points` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "total_points";
