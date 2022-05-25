4bca780c18d5c4f9a47d9a6d9aa00a4de20a0b53
Rohan Jadvani <jadvanirohan@gmail.com>
2022-05-25 14:44:03 -0400

    feat(api): Add read client for ranking queries

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, PrismaPromise } from '@prisma/client';
 import { ApiConfigService } from '../api-config/api-config.service';
 
 @Injectable()
@@ -10,23 +10,38 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  private readonly readClient: PrismaClient;
+
   constructor(readonly config: ApiConfigService) {
     super({
       datasources: {
         db: {
-          url: `${config.get<string>(
-            'DATABASE_CONNECTION_POOL_URL',
-          )}?pgbouncer=true`,
+          url: config.dbPoolUrl,
         },
       },
     });
+    this.readClient = new PrismaClient({
+      datasources: { db: { url: config.readDbPoolUrl } },
+    });
+  }
+
+  $queryRawUnsafe<T = unknown>(
+    query: string,
+    ...values: unknown[]
+  ): PrismaPromise<T> {
+    if (this.config.isProduction()) {
+      return this.readClient.$queryRawUnsafe<T>(query, ...values);
+    }
+    return super.$queryRawUnsafe<T>(query, ...values);
   }
 
   async onModuleInit(): Promise<void> {
     await this.$connect();
+    await this.readClient.$connect();
   }
 
   async onModuleDestroy(): Promise<void> {
     await this.$disconnect();
+    await this.readClient.$disconnect();
   }
 }
