6bd78f57e5bde359eeab05712009578395cf52b1
Jason Spafford <nullprogrammer@gmail.com>
2022-06-21 16:10:26 -0400

    Expose health/admin with queued job count (#920)
    * Expose health/admin with queued job count
    
    We can use this in data dog to set up alerts for when our queued job
    count gets too high.
    
    * Update src/graphile-worker/graphile-worker.service.ts
    
    Co-authored-by: Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
    
    * Update src/health/health.controller.ts
    
    Co-authored-by: Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
    
    Co-authored-by: Jason Spafford <jasonspafford@Jasons-MacBook-Pro.local>
    Co-authored-by: Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -3,13 +3,17 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import request from 'supertest';
+import { ApiConfigService } from '../api-config/api-config.service';
 import { bootstrapTestApp } from '../test/test-app';
 
 describe('HealthController', () => {
   let app: INestApplication;
+  let API_KEY: string;
 
   beforeAll(async () => {
     app = await bootstrapTestApp();
+    const config = app.get(ApiConfigService);
+    API_KEY = config.get<string>('IRONFISH_API_KEY');
     await app.init();
   });
 
@@ -20,6 +24,19 @@ describe('HealthController', () => {
   describe('GET /health', () => {
     it('returns a 200 status code', async () => {
       await request(app.getHttpServer()).get('/health').expect(HttpStatus.OK);
+    });
+  });
+
+  describe('GET /health/admin', () => {
+    it('returns a 200 status code', async () => {
+      const { body } = await request(app.getHttpServer())
+        .get('/health/admin')
+        .set('Authorization', `Bearer ${API_KEY}`)
+        .expect(HttpStatus.OK);
+
+      expect(body).toMatchObject({
+        jobQueueCount: expect.any(Number),
+      });
     });
   });
 });
