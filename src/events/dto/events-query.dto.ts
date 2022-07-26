cb71a1235871f9bd04e73ca7ef8feefbf2ce502e
Skakruk <skakruk15@gmail.com>
2022-07-26 22:40:49 +0300

    Use occurred_at field as cursor

--- src/events/dto/events-query.dto.ts
+++ src/events/dto/events-query.dto.ts
@@ -3,14 +3,49 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsInt, IsOptional, Max } from 'class-validator';
-import { PaginationArgsDto } from '../../common/dto/pagination-args.dto';
+import {
+  IsInt,
+  IsISO8601,
+  IsNumber,
+  IsOptional,
+  Max,
+  Min,
+} from 'class-validator';
 
-export class EventsQueryDto extends PaginationArgsDto {
+export class EventsQueryDto {
   @ApiPropertyOptional({ description: 'Unique User identifier' })
   @IsInt()
   @IsOptional()
   @Max(Number.MAX_SAFE_INTEGER)
   @Type(() => Number)
   readonly user_id?: number;
+
+  @ApiPropertyOptional({
+    description:
+      'An object identifier to use as a cursor pagination to fetch records after',
+  })
+  @IsISO8601()
+  @IsOptional()
+  @Type(() => String)
+  readonly after?: string;
+
+  @ApiPropertyOptional({
+    description:
+      'An object identifier to use as a cursor pagination to fetch records before',
+  })
+  @ApiPropertyOptional()
+  @IsISO8601()
+  @IsOptional()
+  @Type(() => String)
+  readonly before?: string;
+
+  @ApiPropertyOptional({
+    description: 'A limit on the number of objects to return between 1 and 100',
+  })
+  @Max(100)
+  @Min(1)
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  readonly limit?: number;
 }
--- src/events/interfaces/list-events-options.ts
+++ src/events/interfaces/list-events-options.ts
@@ -1,8 +1,10 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { PaginationOptions } from '../../common/interfaces/pagination-options';
 
-export interface ListEventsOptions extends PaginationOptions {
+export interface ListEventsOptions {
+  after?: string;
+  before?: string;
   userId?: number;
+  limit?: number;
 }
