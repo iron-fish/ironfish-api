4db80399416cf621a2cc6e1ab57b766702b4e10d
Jason Spafford <nullprogrammer@gmail.com>
2022-11-02 14:14:34 -0400

    Increase timeout for uploading blocks from the syncer (#1079)

--- src/blocks-transactions-loader/blocks-transactions-loader.ts
+++ src/blocks-transactions-loader/blocks-transactions-loader.ts
@@ -113,7 +113,7 @@ export class BlocksTransactionsLoader {
         {
           // We increased this from the default of 5000 because the transactions were
           // timing out and failing to upsert blocks
-          timeout: 20000,
+          timeout: 30000,
         },
       );
     }
