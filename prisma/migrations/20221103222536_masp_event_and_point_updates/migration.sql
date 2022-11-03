ee56dedc1fa93bce7baa7b47ed44d471182b9a26
Joseph Parks <jowparks@gmail.com>
2022-11-03 15:26:15 -0700

    feat: model updates based on PR review

--- prisma/migrations/20221103222536_masp_event_and_point_updates/migration.sql
+++ prisma/migrations/20221103222536_masp_event_and_point_updates/migration.sql
@@ -1,7 +1,8 @@
 /*
   Warnings:
 
-  - A unique constraint covering the columns `[transaction_hash,graffiti,type]` on the table `masp_transactions` will be added. If there are existing duplicate values, this will fail.
+  - You are about to drop the column `type` on the `masp_transactions` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[transaction_hash,graffiti]` on the table `masp_transactions` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `graffiti` to the `masp_transactions` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -18,7 +19,8 @@ ALTER TYPE "event_type" ADD VALUE 'MASP_BURN';
 ALTER TYPE "event_type" ADD VALUE 'MASP_MINT';
 
 -- AlterTable
-ALTER TABLE "masp_transactions" ADD COLUMN     "graffiti" VARCHAR NOT NULL;
+ALTER TABLE "masp_transactions" DROP COLUMN "type",
+ADD COLUMN     "graffiti" VARCHAR NOT NULL;
 
 -- AlterTable
 ALTER TABLE "user_points" ADD COLUMN     "masp_burn_count" INTEGER NOT NULL DEFAULT 0,
@@ -31,5 +33,8 @@ ADD COLUMN     "masp_transfer_count" INTEGER NOT NULL DEFAULT 0,
 ADD COLUMN     "masp_transfer_last_occurred_at" TIMESTAMP(6),
 ADD COLUMN     "masp_transfer_points" INTEGER NOT NULL DEFAULT 0;
 
+-- DropEnum
+DROP TYPE "MaspTransactionType";
+
 -- CreateIndex
-CREATE UNIQUE INDEX "uq_masp_on_transaction_hash_graffiti_type" ON "masp_transactions"("transaction_hash", "graffiti", "type");
+CREATE UNIQUE INDEX "uq_masp_on_transaction_hash_graffiti" ON "masp_transactions"("transaction_hash", "graffiti");
