1174fd3eb8324507e42fda24474a3a461e2d2a30
Joe <jowparks@gmail.com>
2022-11-22 16:13:36 -0800

    feat: add migration

---
+++ prisma/migrations/20221123000635_unique_weekly_masp_events/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[user_id,type,week]` on the table `events` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "events" ADD COLUMN     "week" INTEGER;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "uq_masp_on_user_id_type_week" ON "events"("user_id", "type", "week");
