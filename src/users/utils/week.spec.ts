b5ed00e4dad4a41323ce9d7cec22a2936afeea73
Jason Spafford <nullprogrammer@gmail.com>
2022-11-23 14:50:39 -0500

    Cleaned up the test

--- src/users/utils/week.spec.ts
+++ src/users/utils/week.spec.ts
@@ -6,19 +6,27 @@ import { phase3Week } from './week';
 
 describe('Week Utils', () => {
   it('phase3Week()', () => {
-    const date = new Date();
+    const getWeek = (
+      yearUTC: number,
+      monthUTC: number,
+      dayUTC: number,
+    ): { day: number; week: number } => {
+      const date = new Date();
+      date.setUTCFullYear(yearUTC, monthUTC, dayUTC);
 
-    const getWeek = (dayOfMonthUTC: number): { day: number; week: number } => {
-      date.setUTCFullYear(2022, 0, dayOfMonthUTC);
-      return { day: date.getUTCDay(), week: phase3Week(date) };
+      return {
+        day: date.getUTCDay(),
+        week: phase3Week(date),
+      };
     };
 
-    expect(getWeek(1)).toEqual({ day: 6, week: 162769 });
-    expect(getWeek(2)).toEqual({ day: 0, week: 162770 });
-    expect(getWeek(3)).toEqual({ day: 1, week: 162770 });
-    expect(getWeek(4)).toEqual({ day: 2, week: 162770 });
-    expect(getWeek(5)).toEqual({ day: 3, week: 162770 });
-    expect(getWeek(6)).toEqual({ day: 4, week: 162770 });
-    expect(getWeek(7)).toEqual({ day: 5, week: 162770 });
+    // (2022, 0, 1) is Sat, 01 Jan 2022 05:00:00 GMT
+    expect(getWeek(2022, 0, 1)).toEqual({ day: 6, week: 162769 });
+    expect(getWeek(2022, 0, 2)).toEqual({ day: 0, week: 162770 });
+    expect(getWeek(2022, 0, 3)).toEqual({ day: 1, week: 162770 });
+    expect(getWeek(2022, 0, 4)).toEqual({ day: 2, week: 162770 });
+    expect(getWeek(2022, 0, 5)).toEqual({ day: 3, week: 162770 });
+    expect(getWeek(2022, 0, 6)).toEqual({ day: 4, week: 162770 });
+    expect(getWeek(2022, 0, 7)).toEqual({ day: 5, week: 162770 });
   });
 });
