598ffac3ed68f18d4ce983cb6b2358af00993b2c
jowparks <jowparks@gmail.com>
2022-12-05 15:01:56 -0800

    fix: bug in MASP leaderboard (#1142)
    * fix: add test
    
    * fix: cleanup test file

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -282,6 +282,66 @@ describe('UsersService', () => {
       expect(records[2].rank).toBe(3);
     });
 
+    it('MASP queries work as expected', async () => {
+      const graffiti = uuid();
+      const now = new Date();
+
+      const userA = await usersService.create({
+        email: faker.internet.email(),
+        graffiti: graffiti + '-a',
+        country_code: faker.address.countryCode('alpha-3'),
+      });
+
+      await eventsService.create({
+        type: EventType.MASP_BURN,
+        userId: userA.id,
+        occurredAt: now,
+        points: 5,
+      });
+      await userPointsService.upsert(
+        await eventsService.getUpsertPointsOptions(userA),
+      );
+
+      await eventsService.create({
+        type: EventType.MASP_MINT,
+        userId: userA.id,
+        occurredAt: new Date(now.valueOf() - 1000),
+        points: 5,
+      });
+      await userPointsService.upsert(
+        await eventsService.getUpsertPointsOptions(userA),
+      );
+
+      await eventsService.create({
+        type: EventType.MASP_BURN,
+        userId: userA.id,
+        occurredAt: new Date(now.valueOf() + 1000),
+        points: 5,
+      });
+      await userPointsService.upsert(
+        await eventsService.getUpsertPointsOptions(userA),
+      );
+
+      const { data: mintRecords } = await usersService.listWithRank({
+        eventType: 'MASP_MINT',
+        search: graffiti,
+        limit: 1,
+      });
+      expect(mintRecords).toHaveLength(1);
+      const { data: burnRecords } = await usersService.listWithRank({
+        eventType: 'MASP_BURN',
+        search: graffiti,
+        limit: 1,
+      });
+      expect(burnRecords).toHaveLength(1);
+      const { data: transferRecords } = await usersService.listWithRank({
+        eventType: 'MASP_TRANSFER',
+        search: graffiti,
+        limit: 1,
+      });
+      expect(transferRecords).toHaveLength(1);
+    });
+
     describe(`when 'event_type' is provided`, () => {
       it('returns a chunk of users by event when specified', async () => {
         const { data: records } = await usersService.listWithRank({
