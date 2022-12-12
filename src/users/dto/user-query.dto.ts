3f9545a07876ec824eb5e0a822029cb6c37e3e8d
Jason Spafford <nullprogrammer@gmail.com>
2022-12-12 12:02:07 -0800

    Fix build errors

--- src/users/dto/user-query.dto.ts
+++ src/users/dto/user-query.dto.ts
@@ -2,17 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { Transform, TransformFnParams } from 'class-transformer';
-import { IsOptional, IsString } from 'class-validator';
-import { stringToBoolean } from '../../common/utils/boolean';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class UserQueryDto {
   @ApiProperty({ description: 'User graffiti' })
   @IsString()
   readonly graffiti!: string;
-
-  @ApiPropertyOptional({
-    description: 'Whether or not to include user rank in the response',
-  })
 }
