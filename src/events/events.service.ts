e26a4dcf08e31a829eb1d542d7fc416fb4c3d85b
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-01-17 12:15:19 -0500

    feat(api): Remove throttling when updating points (#1178)
    * feat(api): Remove throttling when updating points
    
    * test(ironfish): Remove runAt

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -628,11 +628,7 @@ export class EventsService {
 
   addUpdateLatestPointsJob(userId: number, type: EventType): Promise<Job> {
     const updateLatestPointsQueues = 4;
-    const maxJobFrequencyMinutes = 10;
-
     const queueNumber = Math.floor(Math.random() * updateLatestPointsQueues);
-    const runAt = new Date();
-    runAt.setMinutes(runAt.getMinutes() + maxJobFrequencyMinutes);
 
     return this.graphileWorkerService.addJob(
       GraphileWorkerPattern.UPDATE_LATEST_POINTS,
@@ -641,7 +637,6 @@ export class EventsService {
         jobKey: `ulp:${userId}:${type}`,
         jobKeyMode: `preserve_run_at`,
         queueName: `update_latest_points_${queueNumber}`,
-        runAt,
       },
     );
   }
