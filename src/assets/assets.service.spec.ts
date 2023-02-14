b8a03211499e593dff89f50788ce945015cffce7
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-02-14 16:56:38 -0500

    feat(api): Create assets module (#1228)
    * feat(api): Create assets module
    
    * test(api): Add module to test app
    
    * refactor(api): Remove prisma dependency

---
+++ src/assets/assets.service.spec.ts
@@ -0,0 +1,203 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { INestApplication, NotFoundException } from '@nestjs/common';
+import { AssetsService } from '../assets/assets.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { bootstrapTestApp } from '../test/test-app';
+import { TransactionsService } from '../transactions/transactions.service';
+
+describe('AssetsService', () => {
+  let app: INestApplication;
+  let assetsService: AssetsService;
+  let prisma: PrismaService;
+  let transactionsService: TransactionsService;
+
+  beforeAll(async () => {
+    app = await bootstrapTestApp();
+    assetsService = app.get(AssetsService);
+    prisma = app.get(PrismaService);
+    transactionsService = app.get(TransactionsService);
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('findOrThrow', () => {
+    describe('with a missing identifier', () => {
+      it('throws a NotFoundException', async () => {
+        await expect(
+          assetsService.findByIdentifierOrThrow('alskdfj', prisma),
+        ).rejects.toThrow(NotFoundException);
+      });
+    });
+
+    describe('for a valid identifier', () => {
+      it('returns the record', async () => {
+        const transaction = (
+          await transactionsService.createMany([
+            {
+              fee: 0,
+              hash: 'foo',
+              notes: [],
+              size: 0,
+              spends: [],
+            },
+          ])
+        )[0];
+
+        const asset = await assetsService.upsert(
+          {
+            identifier: 'foo',
+            metadata: 'test',
+            name: 'bar',
+            owner: 'baz',
+          },
+          transaction,
+          prisma,
+        );
+
+        expect(
+          await assetsService.findByIdentifierOrThrow(asset.identifier, prisma),
+        ).toEqual(asset);
+      });
+    });
+  });
+
+  describe('upsert', () => {
+    describe('with the first transaction', () => {
+      it('upserts a new record', async () => {
+        const transaction = (
+          await transactionsService.createMany([
+            {
+              fee: 0,
+              hash: 'foo',
+              notes: [],
+              size: 0,
+              spends: [],
+            },
+          ])
+        )[0];
+
+        const identifier = 'fooasdf';
+        const metadata = 'test';
+        const name = 'bar';
+        const owner = 'baz';
+        const asset = await assetsService.upsert(
+          {
+            identifier,
+            metadata,
+            name,
+            owner,
+          },
+          transaction,
+          prisma,
+        );
+
+        expect(asset).toMatchObject({
+          identifier,
+          metadata,
+          name,
+          owner,
+          created_transaction_id: transaction.id,
+        });
+      });
+    });
+
+    describe('with a subsequent transaction', () => {
+      it('does not update the record', async () => {
+        const transaction = (
+          await transactionsService.createMany([
+            {
+              fee: 0,
+              hash: 'foo',
+              notes: [],
+              size: 0,
+              spends: [],
+            },
+          ])
+        )[0];
+
+        const identifier = 'fooasdf';
+        const metadata = 'test';
+        const name = 'bar';
+        const owner = 'baz';
+        await assetsService.upsert(
+          {
+            identifier,
+            metadata,
+            name,
+            owner,
+          },
+          transaction,
+          prisma,
+        );
+
+        const secondTransaction = (
+          await transactionsService.createMany([
+            {
+              fee: 0,
+              hash: 'foo',
+              notes: [],
+              size: 0,
+              spends: [],
+            },
+          ])
+        )[0];
+
+        const asset = await assetsService.upsert(
+          {
+            identifier,
+            metadata,
+            name,
+            owner,
+          },
+          secondTransaction,
+          prisma,
+        );
+        expect(asset).toMatchObject({
+          identifier,
+          metadata,
+          name,
+          owner,
+          created_transaction_id: transaction.id,
+        });
+      });
+    });
+  });
+
+  describe('updateSupply', () => {
+    it('adjusts the supply with the provided delta', async () => {
+      const transaction = (
+        await transactionsService.createMany([
+          {
+            fee: 0,
+            hash: 'foo',
+            notes: [],
+            size: 0,
+            spends: [],
+          },
+        ])
+      )[0];
+
+      const identifier = 'id';
+      const supply = BigInt(10);
+      let asset = await prisma.asset.create({
+        data: {
+          created_transaction_id: transaction.id,
+          identifier,
+          metadata: 'foo',
+          name: 'bar',
+          owner: 'baz',
+          supply,
+        },
+      });
+
+      const delta = -BigInt(3);
+      asset = await assetsService.updateSupply(asset, delta, prisma);
+      expect(asset.supply).toEqual(supply + delta);
+    });
+  });
+});
---
+++ src/assets/assets.service.ts
@@ -0,0 +1,61 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BasePrismaClient } from '../prisma/types/base-prisma-client';
+import { CreateAssetOptions } from './interfaces/create-asset-options';
+import { Asset, Transaction } from '.prisma/client';
+
+@Injectable()
+export class AssetsService {
+  async findByIdentifierOrThrow(
+    identifier: string,
+    prisma: BasePrismaClient,
+  ): Promise<Asset> {
+    const record = await prisma.asset.findUnique({
+      where: {
+        identifier,
+      },
+    });
+    if (!record) {
+      throw new NotFoundException();
+    }
+    return record;
+  }
+
+  async upsert(
+    options: CreateAssetOptions,
+    transaction: Transaction,
+    prisma: BasePrismaClient,
+  ): Promise<Asset> {
+    return prisma.asset.upsert({
+      create: {
+        identifier: options.identifier,
+        metadata: options.metadata,
+        name: options.name,
+        owner: options.owner,
+        supply: BigInt(0),
+        created_transaction_id: transaction.id,
+      },
+      update: {},
+      where: {
+        identifier: options.identifier,
+      },
+    });
+  }
+
+  async updateSupply(
+    asset: Asset,
+    delta: bigint,
+    prisma: BasePrismaClient,
+  ): Promise<Asset> {
+    return prisma.asset.update({
+      data: {
+        supply: asset.supply + delta,
+      },
+      where: {
+        id: asset.id,
+      },
+    });
+  }
+}
