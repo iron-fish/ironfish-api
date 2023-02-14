3ef1c51e7048cee2c74d4b37026ab2d247242081
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-02-14 13:44:14 -0500

    feat(api): Add asset descriptions module

---
+++ src/asset-descriptions/asset-descriptions.service.spec.ts
@@ -0,0 +1,72 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { INestApplication } from '@nestjs/common';
+import { AssetDescriptionType } from '@prisma/client';
+import { AssetsService } from '../assets/assets.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { bootstrapTestApp } from '../test/test-app';
+import { TransactionsService } from '../transactions/transactions.service';
+import { AssetDescriptionsService } from './asset-descriptions.service';
+
+describe('AssetDescriptionsService', () => {
+  let app: INestApplication;
+  let assetDescriptionsService: AssetDescriptionsService;
+  let assetsService: AssetsService;
+  let prisma: PrismaService;
+  let transactionsService: TransactionsService;
+
+  beforeAll(async () => {
+    app = await bootstrapTestApp();
+    assetDescriptionsService = app.get(AssetDescriptionsService);
+    assetsService = app.get(AssetsService);
+    prisma = app.get(PrismaService);
+    transactionsService = app.get(TransactionsService);
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('create', () => {
+    it('creates an asset description record', async () => {
+      const transaction = (
+        await transactionsService.createMany([
+          {
+            fee: 0,
+            hash: 'foo',
+            notes: [],
+            size: 0,
+            spends: [],
+          },
+        ])
+      )[0];
+
+      const asset = await assetsService.upsert(
+        {
+          identifier: 'foo',
+          metadata: 'test',
+          name: 'bar',
+          owner: 'baz',
+        },
+        transaction,
+        prisma,
+      );
+
+      const assetDescription = await assetDescriptionsService.create(
+        AssetDescriptionType.MINT,
+        BigInt(1),
+        asset,
+        transaction,
+        prisma,
+      );
+      expect(assetDescription).toMatchObject({
+        type: AssetDescriptionType.MINT,
+        value: BigInt(1),
+        asset_id: asset.id,
+        transaction_id: transaction.id,
+      });
+    });
+  });
+});
---
+++ src/asset-descriptions/asset-descriptions.service.ts
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { Injectable } from '@nestjs/common';
+import {
+  Asset,
+  AssetDescription,
+  AssetDescriptionType,
+  Transaction,
+} from '@prisma/client';
+import { BasePrismaClient } from '../prisma/types/base-prisma-client';
+
+@Injectable()
+export class AssetDescriptionsService {
+  async create(
+    type: AssetDescriptionType,
+    value: bigint,
+    asset: Asset,
+    transaction: Transaction,
+    prisma: BasePrismaClient,
+  ): Promise<AssetDescription> {
+    return prisma.assetDescription.create({
+      data: {
+        type,
+        value,
+        asset_id: asset.id,
+        transaction_id: transaction.id,
+      },
+    });
+  }
+}
