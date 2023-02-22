0fd50aefd9acde4d2f4e3ae82031dda6cba34384
Jason Spafford <nullprogrammer@gmail.com>
2023-02-22 12:25:36 -0500

    Remove graffit index changes

--- prisma/migrations/20230222171607_add_faucet_index/migration.sql
+++ prisma/migrations/20230222171607_add_faucet_index/migration.sql
@@ -1,11 +1,5 @@
--- DropIndex
-DROP INDEX "index_users_on_graffiti";
-
 -- CreateIndex
 CREATE INDEX "index_faucet_transactions_on_completed_at" ON "faucet_transactions"("completed_at");
 
 -- CreateIndex
 CREATE INDEX "index_faucet_transactions_on_completed_at_and_started_at" ON "faucet_transactions"("started_at", "completed_at");
-
--- CreateIndex
-CREATE INDEX "index_users_on_graffiti" ON "users"("graffiti");
