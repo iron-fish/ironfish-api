12075d6b9cfc5a4a83ca721e2f0074768a66aeb2
Jason Spafford <nullprogrammer@gmail.com>
2023-03-02 13:42:19 -0800

    Fixed tests

---
+++ src/jumio-kyc/fixtures/workflow.ts
@@ -0,0 +1,266 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+export const WORKFLOW_RETRIEVE_FIXTURE = {
+  workflow: {
+    id: 'fakeworkflowid',
+    status: 'PROCESSED',
+    definitionKey: '10013',
+    customerInternalReference: 'FOO',
+  },
+  account: {
+    id: 'fakeaccountid',
+  },
+  createdAt: '2023-03-02T18:48:37.319Z',
+  startedAt: '2023-03-02T19:04:42.008Z',
+  completedAt: '2023-03-02T19:05:00.687Z',
+  credentials: [
+    {
+      id: 'fakecredentialsid',
+      category: 'ID',
+      parts: [
+        {
+          classifier: 'FRONT',
+          href: 'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/credentials/fakecredentialsid/parts/FRONT',
+        },
+        {
+          classifier: 'BACK',
+          href: 'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/credentials/fakecredentialsid/parts/BACK',
+        },
+      ],
+    },
+    {
+      id: 'fakecredentialsid',
+      category: 'FACEMAP',
+      parts: [
+        {
+          classifier: 'FACEMAP',
+        },
+        {
+          classifier: 'LIVENESS_1',
+          href: 'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/credentials/fakecredentialsid/parts/LIVENESS_1',
+        },
+        {
+          classifier: 'LIVENESS_3',
+          href: 'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/credentials/fakecredentialsid/parts/LIVENESS_3',
+        },
+        {
+          classifier: 'LIVENESS_2',
+          href: 'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/credentials/fakecredentialsid/parts/LIVENESS_2',
+        },
+        {
+          classifier: 'LIVENESS_5',
+          href: 'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/credentials/fakecredentialsid/parts/LIVENESS_5',
+        },
+        {
+          classifier: 'LIVENESS_4',
+          href: 'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/credentials/fakecredentialsid/parts/LIVENESS_4',
+        },
+        {
+          classifier: 'LIVENESS_6',
+          href: 'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/credentials/fakecredentialsid/parts/LIVENESS_6',
+        },
+      ],
+    },
+    {
+      id: 'fakecredentialsid',
+      category: 'SELFIE',
+      parts: [
+        {
+          classifier: 'FACE',
+          href: 'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/credentials/fakecredentialsid/parts/FACE',
+        },
+      ],
+    },
+  ],
+  decision: {
+    type: 'PASSED',
+    details: {
+      label: 'PASSED',
+    },
+    risk: {
+      score: 50,
+    },
+  },
+  steps: {
+    href: 'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/workflow-executions/fakeworkflowid/steps',
+  },
+  capabilities: {
+    extraction: [
+      {
+        id: '40cb204c-f7ff-43e7-864b-064dcc8aba85',
+        credentials: [
+          {
+            id: 'fakecredentialsid',
+            category: 'ID',
+          },
+        ],
+        decision: {
+          type: 'PASSED',
+          details: {
+            label: 'OK',
+          },
+        },
+        data: {
+          type: 'ID_CARD',
+          subType: 'NATIONAL_ID',
+          issuingCountry: 'CHL',
+          firstName: 'MUMBO',
+          lastName: 'JUMBO',
+          dateOfBirth: '1970-01-01',
+          expiryDate: '2050-01-01',
+          documentNumber: '1111111',
+          optionalMrzField1: 'Z11',
+          optionalMrzField2: '11111111',
+          currentAge: '70',
+        },
+      },
+    ],
+    similarity: [
+      {
+        id: '594b92df-ae55-4123-883e-ce2724bda94d',
+        credentials: [
+          {
+            id: 'fakecredentialsid',
+            category: 'ID',
+          },
+          {
+            id: 'fakecredentialsid',
+            category: 'SELFIE',
+          },
+        ],
+        decision: {
+          type: 'PASSED',
+          details: {
+            label: 'MATCH',
+          },
+        },
+        data: {
+          similarity: 'MATCH',
+        },
+      },
+    ],
+    liveness: [
+      {
+        id: '3bfa4f49-148d-4361-acc3-10928f69562a',
+        validFaceMapForAuthentication:
+          'https://retrieval.amer-1.jumio.ai/api/v1/accounts/fakeaccountid/credentials/fakecredentialsid/parts/FACEMAP',
+        credentials: [
+          {
+            id: 'fakecredentialsid',
+            category: 'FACEMAP',
+          },
+          {
+            id: 'fakecredentialsid',
+            category: 'SELFIE',
+          },
+        ],
+        decision: {
+          type: 'PASSED',
+          details: {
+            label: 'OK',
+          },
+        },
+        data: {
+          type: 'IPROOV_STANDARD',
+          predictedAge: 31,
+          ageConfidenceRange: '21-41',
+        },
+      },
+    ],
+    dataChecks: [
+      {
+        id: '07d10b79-b84a-454d-94ac-8b80d53f208a',
+        credentials: [
+          {
+            id: 'fakecredentialsid',
+            category: 'ID',
+          },
+        ],
+        decision: {
+          type: 'PASSED',
+          details: {
+            label: 'OK',
+          },
+        },
+      },
+    ],
+    imageChecks: [
+      {
+        id: '1568893e-5edc-453c-9c50-e47fa10578f8',
+        credentials: [
+          {
+            id: 'fakecredentialsid',
+            category: 'ID',
+          },
+          {
+            id: 'fakecredentialsid',
+            category: 'SELFIE',
+          },
+        ],
+        decision: {
+          type: 'WARNING',
+          details: {
+            label: 'REPEATED_FACE',
+          },
+        },
+        data: {
+          faceSearchFindings: {
+            status: 'DONE',
+            findings: [
+              'f7fe3c49-6221-4d87-b8b9-0cd243b65088',
+              '85b26f35-cf4e-4a68-8bce-88c20f06d3ff',
+            ],
+          },
+        },
+      },
+    ],
+    usability: [
+      {
+        id: 'ab66e806-87a7-4132-a677-8ae594d21fbd',
+        credentials: [
+          {
+            id: 'fakecredentialsid',
+            category: 'ID',
+          },
+        ],
+        decision: {
+          type: 'PASSED',
+          details: {
+            label: 'OK',
+          },
+        },
+      },
+      {
+        id: '93e6c38c-0917-40ef-9df5-3008fd83717f',
+        credentials: [
+          {
+            id: 'fakecredentialsid',
+            category: 'FACEMAP',
+          },
+        ],
+        decision: {
+          type: 'PASSED',
+          details: {
+            label: 'OK',
+          },
+        },
+      },
+      {
+        id: '53e1b87a-83f8-4d8b-a97d-6ad5b8bdfe9e',
+        credentials: [
+          {
+            id: 'fakecredentialsid',
+            category: 'SELFIE',
+          },
+        ],
+        decision: {
+          type: 'PASSED',
+          details: {
+            label: 'OK',
+          },
+        },
+      },
+    ],
+  },
+};
--- src/jumio-kyc/kyc.controller.spec.ts
+++ src/jumio-kyc/kyc.controller.spec.ts
@@ -2,7 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { HttpStatus, INestApplication } from '@nestjs/common';
-import { User } from '@prisma/client';
+import { KycStatus, User } from '@prisma/client';
+import assert from 'assert';
+import axios from 'axios';
 import faker from 'faker';
 import request from 'supertest';
 import { v4 as uuid } from 'uuid';
@@ -14,6 +16,7 @@ import { RedemptionService } from '../redemptions/redemption.service';
 import { bootstrapTestApp } from '../test/test-app';
 import { UsersService } from '../users/users.service';
 import { CALLBACK_FIXTURE } from './fixtures/callback';
+import { WORKFLOW_RETRIEVE_FIXTURE } from './fixtures/workflow';
 import { KycService } from './kyc.service';
 import { serializeKyc } from './utils/serialize-kyc';
 
@@ -118,7 +121,7 @@ describe('KycController', () => {
 
   describe('POST /callback', () => {
     it('resolves 200 when transaction found/updated', async () => {
-      // const user = await mockUser();
+      const user = await mockUser();
 
       // create user
       await request(app.getHttpServer())
@@ -129,11 +132,30 @@ describe('KycController', () => {
         })
         .expect(HttpStatus.CREATED);
 
+      let redemption = await redemptionService.findOrThrow(user);
+      assert.ok(redemption.jumio_account_id);
+      expect(redemption.kyc_status).toBe(KycStatus.IN_PROGRESS);
+
+      const transaction = await jumioTransactionService.findLatestOrThrow(user);
+
+      const callbackData = CALLBACK_FIXTURE(
+        redemption.jumio_account_id,
+        transaction.workflow_execution_id,
+        'PROCESSED',
+      );
+
+      jest.spyOn(axios, 'get').mockResolvedValueOnce({
+        data: WORKFLOW_RETRIEVE_FIXTURE,
+      });
+
       await request(app.getHttpServer())
-        .post(`/callback`)
+        .post('/kyc/callback')
         .set('Authorization', 'did-token')
-        .send(CALLBACK_FIXTURE)
-        .expect(HttpStatus.NOT_FOUND);
+        .send(callbackData)
+        .expect(HttpStatus.OK);
+
+      redemption = await redemptionService.findOrThrow(user);
+      expect(redemption.kyc_status).toBe(KycStatus.SUBMITTED);
     });
   });
 
