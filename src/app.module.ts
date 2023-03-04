91bed4508ac36c23e33641607ea2e8e851da8cd4
Jason Spafford <nullprogrammer@gmail.com>
2023-03-03 17:05:22 -0800

    Move banned countries into config (#1294)
    This lets us tune it and test on staging

--- src/app.module.ts
+++ src/app.module.ts
@@ -104,6 +104,7 @@ export const REST_MODULES = [
         MAGIC_SECRET_KEY: joi.string().required(),
         NETWORK_VERSION: joi.number().required(),
         NODE_ENV: joi.string().required(),
+        REDEMPTION_BAN_LIST: joi.string().required(),
         PORT: joi.number().default(8003),
       }),
     }),
