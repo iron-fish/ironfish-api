a3087cbcd9df5d726811ee009b32d6748d7506a1
Joe <jowparks@gmail.com>
2023-03-07 11:37:47 -0800

    feat: add allocations table

---
+++ prisma/migrations/20230308230847_add_pool_allocation_columns/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "redemptions" ADD COLUMN     "pool1_ore" BIGINT,
+ADD COLUMN     "pool2_ore" BIGINT,
+ADD COLUMN     "pool3_ore" BIGINT,
+ADD COLUMN     "pool4_ore" BIGINT;
