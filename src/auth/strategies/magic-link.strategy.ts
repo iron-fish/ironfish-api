3a440eb97b85c81b990a8bb9e619569f52b095df
ygao76 <yajun@ironfish.network>
2023-03-31 12:13:45 -0700

    Add jwt token verification

--- src/auth/strategies/magic-link.strategy.ts
+++ src/auth/strategies/magic-link.strategy.ts
@@ -3,7 +3,9 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import cookie from 'cookie';
 import { Request } from 'express';
+import jwt, { JsonWebTokenError } from 'jsonwebtoken';
 import { Strategy } from 'passport';
 import { ApiConfigService } from '../../api-config/api-config.service';
 import { MagicLinkContext } from '../../common/interfaces/magic-link-context';
@@ -24,14 +26,6 @@ export class MagicLinkStrategy extends PassportStrategy(
   }
 
   async authenticate(req: Request): Promise<void> {
-    const { authorization } = req.headers;
-    if (!authorization) {
-      return this.fail({
-        code: 'no_authorization',
-        message: 'Error: Request header is invalid.',
-      });
-    }
-
     if (this.config.get('DISABLE_LOGIN')) {
       return this.fail({
         code: 'disable_login',
@@ -39,29 +33,80 @@ export class MagicLinkStrategy extends PassportStrategy(
       });
     }
 
+    const { authorization } = req.headers;
+    const cookies = cookie.parse(req.headers.cookie || '');
+
     let email;
-    try {
-      email = await this.magicLinkService.getEmailFromHeader(authorization);
-    } catch {
-      return this.fail({
-        code: 'email_error',
-        message: 'Error: Failed to get the email. Please try again.',
-      });
+    if (authorization) {
+      try {
+        email = await this.magicLinkService.getEmailFromHeader(authorization);
+      } catch {
+        return this.fail({
+          code: 'email_error',
+          message: 'Error: Failed to get the email. Please try again.',
+        });
+      }
+    } else if (cookies) {
+      try {
+        const token: string = cookies.jwt;
+        const decoded = jwt.verify(token, this.config.get('JWT_TOKEN_SECRET'));
+
+        if (decoded.sub === undefined || decoded.sub.toString().length === 0) {
+          return this.fail({
+            code: 'invalid_token',
+            message: 'Error: Jwt token is missing email.',
+          });
+        }
+
+        email = decoded.sub.toString();
+      } catch (err) {
+        if (err instanceof JsonWebTokenError) {
+          switch (err.name) {
+            case 'TokenExpiredError':
+              return this.fail({
+                code: 'token_expired',
+                message: 'Error: Jwt token is expired.',
+              });
+            case 'JsonWebTokenError':
+              return this.fail({
+                code: 'jwt_token_error',
+                message: `Error: Jwt token has error. ${err.message}`,
+              });
+            default:
+              return this.fail({
+                code: 'token_verification_error',
+                message: `Error: Failed to verify token. ${err.name} ${err.message}`,
+              });
+          }
+        } else {
+          return this.fail({
+            code: 'token_error',
+            message: `Error: Failed to verify token.`,
+          });
+        }
+      }
     }
 
-    try {
-      const user = await this.usersService.findByEmailOrThrow(email);
-      req.context = {
-        ...req.context,
-        user,
-      } as MagicLinkContext;
-    } catch {
-      return this.fail({
-        code: 'user_not_found',
-        message: 'Error: No Iron Fish account exists for this email.',
-      });
+    if (email) {
+      try {
+        const user = await this.usersService.findByEmailOrThrow(email);
+        req.context = {
+          ...req.context,
+          user,
+        } as MagicLinkContext;
+      } catch {
+        return this.fail({
+          code: 'user_not_found',
+          message: 'Error: No Iron Fish account exists for this email.',
+        });
+      }
+
+      return this.pass();
     }
 
-    return this.pass();
+    return this.fail({
+      code: 'no_authorization',
+      message: 'Error: Request is missing authorization data.',
+    });
   }
 }
