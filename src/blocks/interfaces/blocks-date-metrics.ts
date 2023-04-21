77eddcb2dc58d66755d7f41fa7f5e5da6f6cd475
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-04-21 11:12:45 -0400

    fix(api): Fix bigint write to prisma for daily charts

--- src/blocks/interfaces/blocks-date-metrics.ts
+++ src/blocks/interfaces/blocks-date-metrics.ts
@@ -3,7 +3,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 export interface BlocksDateMetrics {
   averageBlockTimeMs: number;
-  averageDifficultyMillis: number;
+  averageDifficultyMillis: bigint;
   blocksCount: number;
   blocksWithGraffitiCount: number;
   chainSequence: number;
