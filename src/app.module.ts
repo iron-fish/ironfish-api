76a7a67e967dbefc1a017b991b8a45023dfaf36d
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-05-09 12:36:56 -0400

    feat(api): Remove events

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,11 +21,9 @@ import { BlocksDailyRestModule } from './blocks-daily/blocks-daily.rest.module';
 import { ContextMiddleware } from './common/middlewares/context.middleware';
 import { RequireSslMiddleware } from './common/middlewares/require-ssl.middleware';
 import { DatadogModule } from './datadog/datadog.module';
-import { EventsRestModule } from './events/events.rest.module';
 import { FaucetTransactionsRestModule } from './faucet-transactions/faucet-transactions.rest.module';
 import { HealthRestModule } from './health/health.rest.module';
 import { LoggerModule } from './logger/logger.module';
-import { MetricsRestModule } from './metrics/metrics.rest.module';
 import { TelemetryRestModule } from './telemetry/telemetry.rest.module';
 import { TransactionsRestModule } from './transactions/transactions.rest.module';
 import { VersionsRestModule } from './versions/versions.rest.module';
@@ -38,10 +36,8 @@ export const REST_MODULES = [
   AuthRestModule,
   BlocksDailyRestModule,
   BlocksRestModule,
-  EventsRestModule,
   FaucetTransactionsRestModule,
   HealthRestModule,
-  MetricsRestModule,
   TelemetryRestModule,
   TransactionsRestModule,
   VersionsRestModule,
@@ -54,38 +50,20 @@ export const REST_MODULES = [
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: joi.object({
-        ALLOW_BLOCK_MINED_POINTS: joi.boolean().default(false),
-        ALLOW_NODE_UPTIME_POINTS: joi.boolean().default(false),
         API_URL: joi.string().required(),
         BLOCK_EXPLORER_URL: joi.string().required(),
         BLOCK_LOADER_TRANSACTION_TIMEOUT: joi.number().optional(),
-        CHECK_EVENT_OCCURRED_AT: joi.boolean().default(true),
-        CHECK_USER_CREATED_AT: joi.boolean().default(true),
         DATABASE_CONNECTION_POOL_URL: joi.string().required(),
         DATABASE_URL: joi.string().required(),
         DATADOG_URL: joi.string().required(),
         DISABLE_FAUCET: joi.boolean().default(false),
-        DISABLE_LOGIN: joi.boolean().default(false),
         DYNO: joi.string().allow('').default(''),
-        ENABLE_PHASE_3_END_CHECK: joi.boolean().default(true),
-        ENABLE_SIGNUP: joi.boolean().default(true),
-        ENABLE_USER_UPDATING: joi.boolean().default(true),
         GRAPHILE_CONCURRENCY: joi.number().required(),
-        INCENTIVIZED_TESTNET_URL: joi.string().required(),
         INFLUXDB_API_TOKEN: joi.string().required(),
         INFLUXDB_BUCKET: joi.string().required(),
         INFLUXDB_ORG: joi.string().required(),
         INFLUXDB_URL: joi.string().required(),
         IRONFISH_API_KEY: joi.string().required(),
-        IRONFISH_PHASE_ONE_API_URL: joi.string().required(),
-        IRONFISH_PHASE_TWO_API_URL: joi.string().required(),
-        JUMIO_API_CALLBACK_SECRET: joi.string().required(),
-        JUMIO_API_SECRET: joi.string().required(),
-        JUMIO_API_TOKEN: joi.string().required(),
-        JUMIO_URL: joi.string().required(),
-        JUMIO_WORKFLOW_DEFINITION: joi.number().required(),
-        KYC_MAX_ATTEMPTS: joi.number().required(),
-        JWT_TOKEN_SECRET: joi.string().required(),
         NETWORK_VERSION: joi.number().required(),
         NODE_ENV: joi.string().required(),
         PORT: joi.number().default(8003),
--- src/health/health.rest.module.ts
+++ src/health/health.rest.module.ts
@@ -2,12 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Module } from '@nestjs/common';
-import { EventsModule } from '../events/events.module';
 import { GraphileWorkerModule } from '../graphile-worker/graphile-worker.module';
 import { HealthController } from './health.controller';
 
 @Module({
   controllers: [HealthController],
-  imports: [GraphileWorkerModule, EventsModule],
+  imports: [GraphileWorkerModule],
 })
 export class HealthRestModule {}
