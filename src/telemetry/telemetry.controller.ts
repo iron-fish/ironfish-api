0661764a1f74fab863423b2e5c058058eb423df4
Daniel Cogan <danield9tqh@gmail.com>
2023-05-24 10:53:44 -0700

    Filter out block_assembled telemetry (#1507)

--- src/telemetry/telemetry.controller.ts
+++ src/telemetry/telemetry.controller.ts
@@ -79,7 +79,6 @@ const TELEMETRY_WHITELIST = new Map<string, true | Array<string>>([
     ],
   ],
   ['transaction_propagation', true],
-  ['block_assembled', true],
   ['forks_count', true],
   ['fee_rate_estimate', true],
 ]);
