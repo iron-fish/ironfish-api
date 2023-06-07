31b42147fd8ce0db4a58c19a8f14fc7bae64c1a1
Rohan Jadvani <rohan@hey.com>
2023-06-07 18:30:05 -0400

    feat(api): Return supply properly in response body

--- src/supply/supply.controller.ts
+++ src/supply/supply.controller.ts
@@ -4,8 +4,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 import { BlocksService } from '../blocks/blocks.service';
 
 @ApiTags('Supply')
@@ -15,21 +16,21 @@ export class SupplyController {
 
   @ApiOperation({ summary: 'Gets the circulating supply of the chain' })
   @Get('circulating')
-  async circulating(): Promise<number> {
+  async circulating(@Res() res: Response): Promise<void> {
     const head = await this.blocksService.head();
     const { circulating } = this.blocksService.totalAndCirculatingSupplies(
       head.sequence,
     );
-    return circulating;
+    res.json(circulating);
   }
 
   @ApiOperation({ summary: 'Gets the total supply of the chain' })
   @Get('total')
-  async total(): Promise<number> {
+  async total(@Res() res: Response): Promise<void> {
     const head = await this.blocksService.head();
     const { total } = this.blocksService.totalAndCirculatingSupplies(
       head.sequence,
     );
-    return total;
+    res.json(total);
   }
 }
