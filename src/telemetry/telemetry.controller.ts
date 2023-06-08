796e10d52a4dcd4c8c8942f43b4dff45d745b4dc
ygao76 <yajun@ironfish.network>
2023-06-07 17:56:34 -0700

    Update the name

--- src/telemetry/telemetry.controller.ts
+++ src/telemetry/telemetry.controller.ts
@@ -80,7 +80,7 @@ const TELEMETRY_WHITELIST = new Map<string, true | Array<string>>([
   ],
   ['transaction_propagation', true],
   ['forks_count', true],
-  ['chain_database_size', true],
+  ['chain_db', true],
   ['fee_rate_estimate', true],
 ]);
 
