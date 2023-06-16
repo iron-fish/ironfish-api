ed63f89f4bb5fc91b3bf635a946d9d1206776a47
Andrea Corbellini <andrea@iflabs.network>
2023-06-16 16:29:37 -0700

    Add support for the `verified` parameter to the `/assets` endpoint

--- src/assets/dto/assets-query.dto.ts
+++ src/assets/dto/assets-query.dto.ts
@@ -1,13 +1,21 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsOptional, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform, TransformFnParams } from 'class-transformer';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 import { PaginationArgsDto } from '../../common/dto/pagination-args.dto';
+import { stringToBoolean } from '../../common/utils/boolean';
 
 export class AssetsQueryDto extends PaginationArgsDto {
   @ApiPropertyOptional({ description: 'Keyword search filter' })
   @IsOptional()
   @IsString()
   readonly search?: string;
+
+  @ApiProperty({ description: 'Return only verified or unverified assets' })
+  @IsOptional()
+  @IsBoolean()
+  @Transform(({ value }: TransformFnParams) => stringToBoolean(value))
+  readonly verified?: boolean;
 }
