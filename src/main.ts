62f53087ace718cab14975bef1e36b5b73fa2104
Derek Guenther <derek@iflabs.network>
2024-10-02 12:10:17 -0400

    Allow same origins on both staging and prod

--- src/main.ts
+++ src/main.ts
@@ -38,14 +38,11 @@ async function bootstrap(): Promise<void> {
   const config = app.get(ApiConfigService);
   const logger = app.get(LoggerService);
 
-  const defaultOrigins = [/ironfish.network$/, /localhost/];
-  const enabledOrigins = config.isStaging()
-    ? [
-        ...defaultOrigins,
-        /block-explorer.*ironfish\.vercel\.app$/,
-        /oreowallet-bridge.*ironfish\.vercel\.app$/,
-      ]
-    : defaultOrigins;
+  const enabledOrigins = [
+    /ironfish.network$/,
+    /.*ironfish\.vercel\.app$/,
+    /localhost/,
+  ];
 
   if (config.get<boolean>('CORS_ENABLED')) {
     app.enableCors({
